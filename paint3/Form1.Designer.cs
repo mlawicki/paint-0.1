6a3ed5aa9928e5b4392fb5a0bb37492128bc78a7
mlawicki <michal.lawicki.1988@gmail.com>
2017-07-27 21:47:46 +0200

    Add files via upload
    - line opacity measured more precisely

--- paint3/Form1.Designer.cs
+++ paint3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace paint3
 {
-    partial class Form1
+    partial class Paint
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,377 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otwórzToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.koniecToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edycjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(612, 447);
+            this.panel1.TabIndex = 8;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.Location = new System.Drawing.Point(434, 26);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(65, 60);
+            this.button7.TabIndex = 16;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(8, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 60);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Red;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Location = new System.Drawing.Point(363, 26);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(65, 60);
+            this.button6.TabIndex = 15;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Black;
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.Location = new System.Drawing.Point(501, 26);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(65, 60);
+            this.button8.TabIndex = 17;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Location = new System.Drawing.Point(292, 26);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(65, 60);
+            this.button5.TabIndex = 14;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Aqua;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(79, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 60);
+            this.button2.TabIndex = 11;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button15);
+            this.groupBox1.Controls.Add(this.button14);
+            this.groupBox1.Controls.Add(this.button12);
+            this.groupBox1.Controls.Add(this.button13);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button11);
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.groupBox1.Location = new System.Drawing.Point(566, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(46, 447);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "grubość:";
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(4, 46);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(18, 21);
+            this.button15.TabIndex = 10;
+            this.button15.Text = "-";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(28, 46);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(18, 21);
+            this.button14.TabIndex = 9;
+            this.button14.Text = "+";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(3, 403);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(40, 40);
+            this.button12.TabIndex = 7;
+            this.button12.Text = "wyczyść";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(2, 204);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(41, 40);
+            this.button13.TabIndex = 8;
+            this.button13.Text = "pióro";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(2, 248);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(41, 40);
+            this.button9.TabIndex = 3;
+            this.button9.Text = "kwadrat";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button11
+            // 
+            this.button11.Enabled = false;
+            this.button11.Location = new System.Drawing.Point(2, 340);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(41, 40);
+            this.button11.TabIndex = 5;
+            this.button11.Text = "linia";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.Enabled = false;
+            this.button10.Location = new System.Drawing.Point(2, 294);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(41, 40);
+            this.button10.TabIndex = 4;
+            this.button10.Text = "koło";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Yellow;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Location = new System.Drawing.Point(221, 26);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(65, 60);
+            this.button4.TabIndex = 13;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(150, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 60);
+            this.button3.TabIndex = 12;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.edycjaToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(612, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowyToolStripMenuItem,
+            this.otwórzToolStripMenuItem,
+            this.zapiszToolStripMenuItem,
+            this.koniecToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(34, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // nowyToolStripMenuItem
+            // 
+            this.nowyToolStripMenuItem.Name = "nowyToolStripMenuItem";
+            this.nowyToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.nowyToolStripMenuItem.Text = "Nowy...";
+            this.nowyToolStripMenuItem.Click += new System.EventHandler(this.nowyToolStripMenuItem_Click);
+            // 
+            // otwórzToolStripMenuItem
+            // 
+            this.otwórzToolStripMenuItem.Name = "otwórzToolStripMenuItem";
+            this.otwórzToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.otwórzToolStripMenuItem.Text = "Otwórz";
+            this.otwórzToolStripMenuItem.Click += new System.EventHandler(this.otwórzToolStripMenuItem_Click);
+            // 
+            // zapiszToolStripMenuItem
+            // 
+            this.zapiszToolStripMenuItem.Enabled = false;
+            this.zapiszToolStripMenuItem.Name = "zapiszToolStripMenuItem";
+            this.zapiszToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.zapiszToolStripMenuItem.Text = "Zapisz";
+            this.zapiszToolStripMenuItem.Click += new System.EventHandler(this.zapiszToolStripMenuItem_Click);
+            // 
+            // koniecToolStripMenuItem
+            // 
+            this.koniecToolStripMenuItem.Name = "koniecToolStripMenuItem";
+            this.koniecToolStripMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.koniecToolStripMenuItem.Text = "Koniec";
+            this.koniecToolStripMenuItem.Click += new System.EventHandler(this.koniecToolStripMenuItem_Click);
+            // 
+            // edycjaToolStripMenuItem
+            // 
+            this.edycjaToolStripMenuItem.Name = "edycjaToolStripMenuItem";
+            this.edycjaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.edycjaToolStripMenuItem.Text = "Edycja";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.infoToolStripMenuItem.Text = "Info";
+            this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "10";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Paint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 447);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Paint";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Paint_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem edycjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolStripMenuItem nowyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otwórzToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem koniecToolStripMenuItem;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- paint3/Form1.cs
+++ paint3/Form1.cs
@@ -10,11 +10,209 @@ using System.Windows.Forms;
 
 namespace paint3
 {
-    public partial class Form1 : Form
+    public partial class Paint : Form
     {
-        public Form1()
+
+
+     
+
+        public Paint()
         {
             InitializeComponent();
+        }
+
+        Pen pioro = new Pen(Color.Black, 5);
+        bool rysowanierozpoczete = false;
+        Point poczatek = new Point(0, 0);
+        Point koniec = new Point(0, 0);
+        Graphics g;
+        int figura = 1; //figury: 1 - rysowanie 2 - kolo 3 - kwadrat 4 - linia
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pioro.Color = button1.BackColor;
+
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            rysowanierozpoczete = false;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+
+        private void Paint_Load(object sender, EventArgs e)
+        {
+            g = panel1.CreateGraphics();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            poczatek = e.Location;
+            if (e.Button == MouseButtons.Left)
+                rysowanierozpoczete = true;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (rysowanierozpoczete)
+            koniec = e.Location;
+            g = panel1.CreateGraphics();
+            g.DrawLine(pioro, poczatek, koniec);
+            poczatek = koniec;
+
+
+
+
+        }
+
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            figura = 1;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            figura = 2;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            figura = 4;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            figura = 4;
+            pioro.Width = 10;
+            string grubosclabel = pioro.Width.ToString();
+            label2.Text = grubosclabel;
+
+        }
+
+        private void infoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Paint v. 0.3 \n by mlawicki \n github.com/mlawicki");
+        }
+
+        private void nowyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+
+        private void otwórzToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                panel1.BackgroundImage = Image.FromFile(open.FileName);
+                panel1.BackgroundImageLayout = ImageLayout.Stretch;
+
+            }
+
+            else
+                System.Windows.Forms.MessageBox.Show("nie udało się otworzyć obrazka");
+
+
+        }
+
+        private void koniecToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void zapiszToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+                    System.Windows.Forms.MessageBox.Show("Funkcja jeszcze nieobslugiwana");
+
+
+        }
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            pioro.Color = button1.BackColor;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            pioro.Color = button2.BackColor;
+        }
+
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            pioro.Color = button3.BackColor;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            pioro.Color = button4.BackColor;
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            pioro.Color = button5.BackColor;
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            pioro.Color = button6.BackColor;
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            pioro.Color = button7.BackColor;
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            pioro.Color = button8.BackColor;
+        }
+
+
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (pioro.Width >= 100)
+                label2.Text = "100";
+            else
+            {
+                pioro.Width += 5;
+                string grubosclabel = pioro.Width.ToString();
+                label2.Text = grubosclabel;
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (pioro.Width <= 1)
+                label2.Text = "1";
+
+            else
+            {
+                pioro.Width -= 5;
+                string grubosclabel = pioro.Width.ToString();
+                label2.Text = grubosclabel;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
